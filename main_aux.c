df66d699f6cfbabc853ab9a4f6c1b7a17bff05a1
nadz3107 <45856050+nadz3107@users.noreply.github.com>
2018-12-14 14:25:58 -0800

    Update main_aux.c

--- main_aux.c
+++ main_aux.c
@@ -14,6 +14,29 @@ char* mainAux_readCommand(){
 	return NULL;
 }
 
+/* nadin :
+ * this function receives the number of fixed cells from user. it should be between 0-80
+ */
+int mainAux_getNumberOfFixCells(){
+	int fixed_cells;
+		printf("Please enter the number of cells to fill [0-80]:\n");
+		if (scanf("%d", &fixed_cells) != 1)
+		{
+			printf("Error: scan has failed. Exiting\n");
+			return -1;
+
+		}
+		while ((fixed_cells >80 || fixed_cells < 0))
+		{
+			printf("Error: invalid number of cells to fill (should be between 0 and 80)\n");
+			printf("Please enter the number of cells to fill [0-80]:\n");
+			scanf("%d", &fixed_cells);
+
+		}
+		return fixed_cells;
+	}
+
+
 void printBoard(int**matrix){
 
 }
