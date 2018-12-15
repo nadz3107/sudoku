dfae7cd60e9c83eeca9a7a275d68ab6aa4302c1f
nadz3107 <45856050+nadz3107@users.noreply.github.com>
2018-12-14 16:39:15 -0800

    Update main_aux.c

--- main_aux.c
+++ main_aux.c
@@ -3,7 +3,20 @@
 #include <stdlib.h>
 
 int mainAux_getNumberOfFixCells(){
-	return 0;
+	int fixed_cells;
+	printf("Please enter the number of cells to fill [0-80]:\n");
+	if (scanf("%d", &fixed_cells) != 1)
+	{
+		printf("Error: scan has failed. Exiting\n");
+		return -1;
+	}
+	while ((fixed_cells >80 || fixed_cells < 0))
+	{
+		printf("Error: invalid number of cells to fill (should be between 0 and 80)\n");
+		printf("Please enter the number of cells to fill [0-80]:\n");
+		scanf("%d", &fixed_cells);
+	}
+	return fixed_cells;
 }
 
 char* mainAux_readCommand(){
@@ -14,6 +27,43 @@ char* mainAux_readCommand(){
 	return NULL;
 }
 
-void printBoard(int**matrix){
+/* nadin*/
+void rowSeparator() {
+	int i;
+	for (i = 0; i < 34; i++) {
+		printf("-");
+	}
+	printf("\n");
+}
 
+/*nadin: print the current position of the board */
+
+void printBoard( int **matrixPlay, int **matrixfixed){
+	int i,j;
+	for(i=0; i<9; i++)
+	{
+		/*every 3 rows we will print a line*/
+		if(i % 3 == 0)
+		{
+			rowSeparator();
+		}
+		for(j=0;j<9;j++)
+		{
+			/*every 3 col we will print a line*/
+			if(j % 3 == 0)
+			{
+				printf("| ");
+			}
+			if(matrixfixed[i][j]!=0)
+			{
+				printf(".%d ",matrixPlay[i][j]);
+			}else if(matrixfixed[i][j]==0 && matrixPlay[i][j]!=0)
+			{
+				printf(" %d ",matrixPlay[i][j]);
+			}else
+			{
+				printf("   ");
+			}
+		}printf("|\n");
+	}rowSeparator();
 }
